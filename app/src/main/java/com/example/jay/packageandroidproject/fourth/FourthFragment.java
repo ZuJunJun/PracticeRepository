d38d5a4192a15b7138c0d5cc99e6dd69b8933dd4
ZHU <Zujunjun>
2019-04-12 11:14:08 +0800

    当通知被关闭时去设置页手动打开通知

--- app/src/main/java/com/example/jay/packageandroidproject/fourth/FourthFragment.java
+++ app/src/main/java/com/example/jay/packageandroidproject/fourth/FourthFragment.java
@@ -1,13 +1,19 @@
 package com.example.jay.packageandroidproject.fourth;
 
 
+import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
@@ -43,6 +49,7 @@ public class FourthFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         notificationManager = ((NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE));
         view.findViewById(R.id.category_one).setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
                 sendDefaultNotification();
@@ -56,7 +63,15 @@ public class FourthFragment extends Fragment {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void sendDefaultNotification() {
+        NotificationChannel notificationChannel = notificationManager.getNotificationChannel(Constant.CHAT_ID);
+        if (notificationChannel.getImportance()==NotificationManager.IMPORTANCE_NONE) {
+            Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, getActivity().getPackageName());
+            intent.putExtra(Settings.EXTRA_CHANNEL_ID, notificationChannel.getId());
+            startActivity(intent);
+        }
         Notification notification = new NotificationCompat.Builder(getContext(), Constant.CHAT_ID)
                 .setContentTitle("你收到一条聊天消息")
                 .setContentText("今天中午吃啥呢？")
